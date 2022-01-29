06b1c74b47e88f19bb0e0c0174d9fb5a1e2db2c0
Jon Borgonia <jon@digit.co>
2022-01-29 06:58:51 -0800

    added Title scene
    added HowToPlay scene
    added placeholder Main scene
    all transition buttons are functional
    
    resolves #8

---
+++ title/Title.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+func _ready():
+	pass 
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://main/Main.tscn")
+
+
+func _on_HowToPlayButton_pressed():
+	get_tree().change_scene("res://how_to_play/HowToPlay.tscn")
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
