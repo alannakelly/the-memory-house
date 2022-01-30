126f399c1ce7c335a55e0d80c53ffcb781895ad3
Alanna Kelly <alanna@alannakelly.ie>
2022-01-30 01:05:06 +0000

    starting work on props

--- player/Player.gd
+++ player/Player.gd
@@ -1,7 +1,28 @@
 extends KinematicBody2D
 
+class_name Player
+
 export var speed = 100.0
 var velocity = Vector2()
+var dusting = false;
+onready var sprite = $Sprite
+
+func _process(delta):
+	if Input.is_action_pressed("ui_select"):
+		dusting = true
+	else:
+		dusting = false
+		
+	sprite.flip_h=false
+	if Input.is_action_pressed("ui_up"):
+		sprite.animation = "walk_north"
+	if Input.is_action_pressed("ui_down"):
+		sprite.animation = "walk_south"
+	if Input.is_action_pressed("ui_left"):
+		sprite.animation = "walk_west_east"
+	if Input.is_action_pressed("ui_right"):
+		sprite.flip_h=true
+		sprite.animation = "walk_west_east"
 
 func get_input():
 	velocity = Vector2()
@@ -17,4 +38,3 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity*delta)
-	
--- props/Prop.gd
+++ props/Prop.gd
@@ -1,4 +1,24 @@
 extends Node2D
 
+enum Type {
+	NONE,
+	FLOWER_POT,
+	BOOKSHELF,
+	TABLE,
+	GLASS_OF_WATER,
+	WARDROBE,
+	CAKE_SLICER,
+	DINING_CLOTH,
+	PLAYING_CARDS,
+	INDOOR_PLANTS
+}
+
+var dusted = false;
+var color = Color(0.0,0.0,1.0)
+export var type = Type.NONE
+
 func _on_Collision_body_entered(body):
-	print_debug("hit by: ", body)
+	if body is Player:
+		if body.dusting:
+			dusted = true
+			self.modulate = Color(0.0,1.0,0.5)
